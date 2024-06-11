384b1fb0d454ea1b0d68cfb55145efe4a4b2d6d0
hcskiaServer <hcskia@server.com>
2024-06-11 18:26:15 +0800

    init

---
+++ src/main/java/com/hcskia/maianalyzerapi/MaiAnalyzerApiApplication.java
@@ -0,0 +1,13 @@
+package com.hcskia.maianalyzerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaiAnalyzerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaiAnalyzerApiApplication.class, args);
+    }
+
+}
